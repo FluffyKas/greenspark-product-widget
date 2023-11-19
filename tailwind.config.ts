882b08c613aae4b58215e60be3bb364044e4dc3f
Zsófia Maruzs <zsofia@splendex.io>
2023-11-19 11:23:49 +0100

    chore: add tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,19 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        greenspark_purple: 'hsla(232, 51%, 36%, 1)',
+        greenspark_purple_hover: 'hsla(232, 51%, 36%, 0.8)',
+        greenspark_green: 'hsla(157, 33%, 35%, 1)',
+        greenspark_green_hover: 'hsla(157, 17%, 73%, 1)',
+        greenspark_sand: 'hsla(42, 47%, 90%, 1)',
+        greenspark_grey: 'hsla(0, 0%, 69%, 1)',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;

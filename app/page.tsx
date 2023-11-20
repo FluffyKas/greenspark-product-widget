58f9d33c38525b8dd33745fb8fe8164913fc9690
Zsófia Maruzs <zsofia@splendex.io>
2023-11-20 17:35:13 +0100

    chore: move Tailwind classes to css file to declutter

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,13 @@ import { Widget } from './components/Widget';
 import { useWidgets } from './context/WidgetContext';
 
 export default function Home() {
-  const { widgetData, loading, error } = useWidgets();
+  const { widgets, loading, error } = useWidgets();
 
   return (
     <main>
-      <div className="widget-container | rounded-lg mx-[1.38rem] my-12 md:mx-auto max-w-[53.1875rem]">
-        <h1 className="text-3xl font-bold">Per product widgets</h1>
-        <div className="flex">{widgetData && widgetData.map((widget) => <Widget key={widget.id} widget={widget} />)}</div>
+      <div className="widget-container">
+        <h1 className="widget-container-title">Per product widgets</h1>
+        <div className="widgets-inner-container">{widgets && widgets.map((widget) => <Widget key={widget.id} widget={widget} />)}</div>
       </div>
     </main>
   );

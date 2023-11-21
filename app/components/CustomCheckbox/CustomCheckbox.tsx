451cb87e9ee03119d82658c08bc6088fdeef1294
Zsófia Maruzs <zsofia@splendex.io>
2023-11-21 16:04:02 +0100

    feat: add tooltip

--- app/components/CustomCheckbox/CustomCheckbox.tsx
+++ app/components/CustomCheckbox/CustomCheckbox.tsx
@@ -5,30 +5,45 @@ import './CustomCheckbox.css';
 import IconTooltip from '../../assets/icon-tooltip.svg';
 import Image from 'next/image';
 import { useState } from 'react';
-import { useWidgets } from '@/app/context/WidgetContext';
+import { Tooltip } from '../Tooltip/Tooltip';
 
 export const CustomCheckbox = ({ active }: { active: boolean }) => {
-  const [isChecked, setIsChecked] = useState(false);
   const [usePublicProfile, setUsePublicProfile] = useState<boolean>(active);
+  const [isTooltipVisible, setIsTooltipVisible] = useState(false);
+
+  const toggleTooltip = (e: React.MouseEvent, isVisible: boolean) => {
+    e.preventDefault();
+    setIsTooltipVisible(isVisible);
+  };
 
   return (
-    <form className="checkbox-form">
-      <label className="checkbox-form-label">
-        <Link href="#" className="checkbox-form-link">
-          Link to Public Profile
-        </Link>
-        <button className="tooltip-toggle" aria-label="Click or hover over to display tooltip." onClick={() => setIsChecked(!isChecked)}>
-          <Image src={IconTooltip} alt="" />
-        </button>
-      </label>
-      <input
-        type="checkbox"
-        name="public-profile-checkbox"
-        id="public-profile-checkbox"
-        className="public-profile-checkbox"
-        checked={usePublicProfile}
-        onChange={() => setUsePublicProfile(!usePublicProfile)}
-      />
-    </form>
+    <div>
+      <form className="checkbox-form">
+        <label className="checkbox-form-label">
+          <Link href="#" className="checkbox-form-link">
+            Link to Public Profile
+          </Link>
+          <button
+            className="tooltip-toggle"
+            aria-label="Click or hover over to display tooltip."
+            aria-describedby="tooltip"
+            onClick={(e) => toggleTooltip(e, !isTooltipVisible)}
+            onMouseEnter={(e) => toggleTooltip(e, true)}
+            onMouseLeave={(e) => toggleTooltip(e, false)}
+          >
+            <Image src={IconTooltip} alt="" />
+          </button>
+          {<Tooltip isTooltipVisible={isTooltipVisible} toggleTooltip={toggleTooltip} />}
+        </label>
+        <input
+          type="checkbox"
+          name="public-profile-checkbox"
+          id="public-profile-checkbox"
+          className="public-profile-checkbox"
+          checked={usePublicProfile}
+          onChange={() => setUsePublicProfile(!usePublicProfile)}
+        />
+      </form>
+    </div>
   );
 };
--- app/components/Tooltip/Tooltip.tsx
+++ app/components/Tooltip/Tooltip.tsx
@@ -1,22 +1,24 @@
-export const Tooltip = () => (
-  <div className="data-[side=bottom]:animate-slideUpAndFade data-[side=right]:animate-slideLeftAndFade data-[side=left]:animate-slideRightAndFade data-[side=top]:animate-slideDownAndFade w-[300px] rounded-md bg-white p-5 shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] data-[state=open]:transition-all">
-    <div className="flex flex-col gap-[7px]">
-      <img className="block h-[60px] w-[60px] rounded-full" src="https://pbs.twimg.com/profile_images/1337055608613253126/r_eiMp2H_400x400.png" alt="Radix UI" />
-      <div className="flex flex-col gap-[15px]">
-        <div>
-          <div className="text-mauve12 m-0 text-[15px] font-medium leading-[1.5]">Radix</div>
-          <div className="text-mauve10 m-0 text-[15px] leading-[1.5]">@radix_ui</div>
-        </div>
-        <div className="text-mauve12 m-0 text-[15px] leading-[1.5]">Components, icons, colors, and templates for building high-quality, accessible UI. Free and open-source.</div>
-        <div className="flex gap-[15px]">
-          <div className="flex gap-[5px]">
-            <div className="text-mauve12 m-0 text-[15px] font-medium leading-[1.5]">0</div> <div className="text-mauve10 m-0 text-[15px] leading-[1.5]">Following</div>
-          </div>
-          <div className="flex gap-[5px]">
-            <div className="text-mauve12 m-0 text-[15px] font-medium leading-[1.5]">2,900</div> <div className="text-mauve10 m-0 text-[15px] leading-[1.5]">Followers</div>
-          </div>
-        </div>
-      </div>
-    </div>
+import Link from 'next/link';
+
+interface TooltipProps {
+  isTooltipVisible: boolean;
+  toggleTooltip: (e: React.MouseEvent, isVisible: boolean) => void;
+}
+
+export const Tooltip = ({ isTooltipVisible, toggleTooltip }: TooltipProps) => (
+  <div
+    className={`tooltip | ${
+      isTooltipVisible ? 'opacity-100' : 'opacity-0'
+    } | absolute bottom-6 left-32 w-[15.5rem] z-10 rounded-sm bg-white px-4 py-6 shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] text-[0.875rem] leading-normal text-center transition-opacity duration-500`}
+    id="tooltip"
+    onMouseEnter={(e) => toggleTooltip(e, true)}
+    onMouseLeave={(e) => toggleTooltip(e, false)}
+  >
+    <p className="mb-3">
+      This widget links directly to your public profile so that you can easily share your impact with your customers. Turn it off here if you do not want the badge to link to it.
+    </p>
+    <Link href="#" className="text-greenspark_green hover:text-greenspark_green_hover duration-200 ease-in">
+      View Public Profile
+    </Link>
   </div>
 );
